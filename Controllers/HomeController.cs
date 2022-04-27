4db17c5054880edd72c7d04db8785fee6875afa0
Fredrik Alm <fredrikalm@Fredriks-MacBook-Pro.local>
2022-04-27 16:07:34 +0200

    first push

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,139 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.Sqlite;
+using blocket_lite.Models;
+using blocket_lite.Models.ProductViewModel;
+
+
+namespace blocket_lite.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        // hämtar alla objekt i databasen 
+        var itemListModel = GetAllItems();
+        return View(itemListModel);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    public IActionResult AddNewProduct()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    //Lägger in values från formulär till databasen.
+     public RedirectResult Insert(ItemModel product)
+   {
+       if(product.category=="vehicle")
+       {
+        using (SqliteConnection con = 
+       new SqliteConnection("Data Source=db.sqlite"))
+       {
+           using (var tableCmd = con.CreateCommand())
+           {
+               con.Open();
+               tableCmd.CommandText = $"INSERT INTO products3 (category,title,price,description,image,miles,year,color) VALUES ('{product.category}','{product.title}','{product.price}','{product.description}','{product.image}','{product.miles}','{product.year}','{product.color}')";
+               try
+               {
+                   tableCmd.ExecuteNonQuery();
+               }
+               catch (Exception ex)
+               {
+                   
+                   Console.WriteLine(ex.Message);
+               }
+           }
+       }
+       }
+       else if (product.category=="cloths")
+       {
+          {
+        using (SqliteConnection con = 
+       new SqliteConnection("Data Source=db.sqlite"))
+       {
+           using (var tableCmd = con.CreateCommand())
+           {
+               con.Open();
+               tableCmd.CommandText = $"INSERT INTO products3 (category,title,price,description,image,gender,size,color) VALUES ('{product.category}','{product.title}','{product.price}','{product.description}','{product.image}','{product.gender}','{product.size}','{product.color}')";
+               try
+               {
+                   tableCmd.ExecuteNonQuery();
+               }
+               catch (Exception ex)
+               {
+                   
+                   Console.WriteLine(ex.Message);
+               }
+           }
+       }
+       } 
+       }
+       
+       return Redirect("https://localhost:7296/");
+   }
+   //hämtar alla produkter från databasen till en lista.
+   internal ItemViewModel GetAllItems()
+    {
+        List<ItemModel> itemList = new();
+
+        using (SqliteConnection con = 
+        new SqliteConnection("Data Source=db.sqlite"))
+        {
+            using (var tableCmd = con.CreateCommand())
+            {
+                con.Open();
+                tableCmd.CommandText= "SELECT * FROM products3 ORDER BY price";
+
+                using (var reader = tableCmd.ExecuteReader())
+                {
+                    if(reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            itemList.Add(
+                                new ItemModel
+                                {
+                                    category= reader.GetString(0),
+                                    title= reader.GetString(1),
+                                    price = reader.GetInt32(2),
+                                    description = reader.GetString(3),
+                                    image = reader.GetString(9)
+                                });
+                        }
+                    }
+                    else 
+                    {
+                        return new ItemViewModel
+                        {
+                            ItemList=itemList
+                        };
+                    }
+                };
+            }
+        }
+
+        return new ItemViewModel 
+        {
+            ItemList= itemList
+        };
+    }
+}
+
---
+++ wwwroot/js/site.js
@@ -0,0 +1,20 @@
+﻿function show_car() {
+    let category = document.getElementById("category").value;
+
+    switch (category) {
+
+        case "vehicle":
+            document.querySelector(".forms_div").style.display = "block";
+            document.getElementById("forms_div").innerHTML = '<div class="form-group"><label asp-for="year"></label><input asp-for="year" class="form-control"></div>'
+            document.getElementById("forms_div").innerHTML = '<div class="form-group"><label asp-for="miles"></label><input asp-for="miles" class="form-control"></div>'
+
+            break;
+        case "cloths":
+
+            break;
+        default:
+            document.querySelector(".forms_div").style.display = "block";
+            break;
+
+    }
+}
