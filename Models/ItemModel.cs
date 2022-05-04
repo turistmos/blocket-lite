b77b2cdab7bffcca4e7de39eaf82922d968cef60
Fredrik Alm <fredrikalm@Fredriks-MacBook-Pro.local>
2022-05-04 10:17:15 +0200

    lagt till username i ItemModel

--- Models/ItemModel.cs
+++ Models/ItemModel.cs
@@ -3,22 +3,22 @@ namespace blocket_lite.Models
 {
     public class ItemModel
     {
-//miles,year,color
-        
-            public string category { get; set; }
-            public string title { get; set;}
-            public int price { get; set;}
-            public string description { get; set; }
+        //miles,year,color
 
-            public int miles {get;set;}
-            public int year {get;set;}
-            public string color {get;set;}
-            public string size {get;set;}
-            public string gender {get;set;}
-            
-            public string image {get;set;}
+        public string category { get; set; }
+        public string title { get; set; }
+        public int price { get; set; }
+        public string description { get; set; }
 
+        public int miles { get; set; }
+        public int year { get; set; }
+        public string color { get; set; }
+        public string size { get; set; }
+        public string gender { get; set; }
 
+        public string image { get; set; }
+
+        public string? username { get; set; }
 
     }
 
